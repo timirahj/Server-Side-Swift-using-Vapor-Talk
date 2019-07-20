157990bfee4ef53e3de8da8e40b1c177f9deb0f3
timirahj <timirah.james@gmail.com>
2019-07-19 17:58:39 -0700

    added corresponding Vapor Demo

---
+++ VaporDemo/FirstWebApp/Sources/App/routes.swift
@@ -0,0 +1,35 @@
+import Vapor
+
+var imageURL: URL?
+
+/// Register your application's routes here.
+public func routes(_ router: Router) throws {
+    // Basic "It works" example
+    router.get { req in
+        return "It works!"
+    }
+    
+    // Basic "Hello, world!" example
+    router.get("hello") { req in
+        return "Hello, there!"
+    }
+    
+    // Instead of stating another string we use a custom PathComponent using parameter
+    router.get("greet", String.parameter) { request -> String in
+        let name = try request.parameters.next(String.self)
+        
+        return "hello \(name)"
+    }
+    
+    struct Character: Encodable {
+        let name: String
+    }
+    
+
+    
+    // Make Leaf your view priority
+    router.get("/") { request -> Future<View> in
+        
+        return try request.view().render("welcome")
+    }
+}
